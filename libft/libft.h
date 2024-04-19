d93827626551217e565ed8af7163e72e02b9b5e2
lareii <lareii@proton.me>
2024-04-19 23:20:03 +0300

    update (libft): func prototypes

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:05:11 by ebabaogl          #+#    #+#             */
-/*   Updated: 2024/04/19 22:46:15 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2024/04/19 23:19:05 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,13 @@
 # include <unistd.h>
 # include <stdio.h>
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-int	ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memset(void *s, int c, size_t n);
+void	ft_bzero(void *s, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
