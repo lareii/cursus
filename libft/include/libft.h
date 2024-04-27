ef9904253c99c96a85de68bf82516c537da52803
lareii <lareii@proton.me>
2024-04-27 03:46:29 +0300

    add (libft): ft_memmove func (pain.)

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:05:11 by ebabaogl          #+#    #+#             */
-/*   Updated: 2024/04/26 18:51:46 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2024/04/27 03:44:25 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
--- libft/src/ft_memmove.c
+++ libft/src/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ebabaogl <ebabaogl@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 14:19:12 by ebabaogl          #+#    #+#             */
-/*   Updated: 2024/04/26 16:52:43 by ebabaogl         ###   ########.fr       */
+/*   Updated: 2024/04/27 03:44:10 by ebabaogl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (dest == src)
+		return (dest);
+	else if (dest < src)
+		dest = ft_memcpy(dest, src, n);
+	else
+	{
+		d = (unsigned char *)dest;
+		s = (const unsigned char *)src;
+		while (n > 0)
+		{
+			d[n] = s[n];
+			n--;
+		}
+	}
 	return (dest);
 }
